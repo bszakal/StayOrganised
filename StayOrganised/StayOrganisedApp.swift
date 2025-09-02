25801d1cf3fad27b65f9b106501908e0985ef1fd
Benjamin <benjamin.szakal1@gmail.com>
2025-09-02 20:35:51 +0200

    Initial Commit

---
+++ StayOrganised/StayOrganisedApp.swift
@@ -0,0 +1,17 @@
+//
+//  StayOrganisedApp.swift
+//  StayOrganised
+//
+//  Created by Benjamin Szakal on 02/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct StayOrganisedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
